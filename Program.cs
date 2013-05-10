280a1c32d2e8cbbd9de968e72dcd48a2e82b9dbc
Dominque Terry <terry_dominque@yahoo.com>
2013-05-10 12:55:17 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*Program: hashtabe example
+ * Programmer: Dominque Terry
+ */
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace hashExample
+{
+    class Program
+    {
+        static Hashtable GetMyTable() 
+        {
+            Hashtable myTable = new Hashtable();
+            myTable.Add("Phone Number", "888-777-9654");
+            myTable.Add("Work Number", "779-999-8525");
+            return myTable;
+        }
+        static void Main(string[] args)
+        {
+            Hashtable myTable = GetMyTable();
+
+            //checks to see if the hashtable contains a key
+            Console.WriteLine(myTable.ContainsKey("Phone Number"));
+
+            //check for another key
+            Console.WriteLine(myTable.Contains("Work Number"));
+
+            string myValue = myTable["Phone Number"].ToString();
+            Console.WriteLine("phone number value: " + myValue);
+
+            string myValue2 = myTable["Work Number"].ToString();
+            Console.WriteLine("work number value: " + myValue2);
+
+
+
+
+
+
+
+        }
+    }
+}
